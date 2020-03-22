a4fe4232827a6821b1eef49c6bd79e08a630fcb6
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-22 13:52:21 +0300

    init:" start typical-arrays-problems task"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined){return 0;}
+  if(array.length == 0){return 0;}
+  var min = array[0];
+  for(var i = 0 ; i < array.length ; i++){
+    if(array[i] < min){ min = array[i]}
+  } 
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined){return 0;}
+  if(array.length == 0){return 0;}
+  var max = array[0];
+  for(var i = 0 ; i < array.length; i++){
+    if(max < array[i]){max = array[i]}
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined){return 0;}
+  if(array.length == 0){return 0;}
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum/array.length;
 }
